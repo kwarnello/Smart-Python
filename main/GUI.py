97784def5515c4616938209be0d735a7d277f938
unknown <kwarnello@gmail.com>
2020-02-01 19:36:09 +0100

    Launcher and main GUI classes created

---
+++ main/GUI.py
@@ -0,0 +1,21 @@
+'''
+Created on 1 lut 2020
+
+@author: Warus
+'''
+
+import tkinter
+
+class MainWindow(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        print("inituje")
+        tkinter._test()
+        
---
+++ main/launcher.py
@@ -0,0 +1,11 @@
+'''
+Created on 1 lut 2020
+
+@author: Warus
+'''
+
+import main.GUI
+
+if __name__ == '__main__':
+    main.GUI.MainWindow()
+    pass
